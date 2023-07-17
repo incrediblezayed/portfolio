c0296b0a3270fad3f04af7025d51e18ef1ff3cf2
Hassan Ansari <hassanansari222@gmail.com>
2023-07-17 17:09:14 +0530

    Update providers.dart

--- lib/src/providers/providers.dart
+++ lib/src/providers/providers.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:portfolio/src/models/enquiry_model.dart';
 import 'package:portfolio/src/providers/main_provider.dart';
-import 'package:portfolio/src/repositories/enquiry_repository.dart';
 import 'package:portfolio/src/repositories/experience_repository.dart';
 import 'package:portfolio/src/repositories/project_repository.dart';
 import 'package:portfolio/src/utils/theme.dart';
@@ -18,10 +16,3 @@ final projectsProvider =
 
 final experiencesProvider =
     FutureProvider((ref) => ExperienceRepository().getExperiences());
-
-/* final addEnquiryProvider =
-    FutureProvider.autoDispose.family<bool, EnquiryModel>(
-  (ref, arg) {
-    return EnquiryRepository().addEnquiry(arg);
-  },
-); */
